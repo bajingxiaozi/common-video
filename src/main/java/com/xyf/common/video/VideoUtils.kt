c88defa8277eda282b6a16e24b6a77c9cb7b231b
getting.xi <getting.xi@foxmail.com>
2020-11-03 23:31:36 +0800

    feat: 更新接口

--- src/main/java/com/xyf/common/video/VideoUtils.kt
+++ src/main/java/com/xyf/common/video/VideoUtils.kt
@@ -2,10 +2,11 @@ package com.xyf.common.video
 
 import com.google.gson.Gson
 import com.xyf.common.annotation.WorkThread
-import com.xyf.common.util.SystemUtils
+import com.xyf.common.util.ProcessUtils
 import com.xyf.common.video.bean.VideoInfoBean
 import org.apache.commons.lang3.StringUtils
 import java.io.File
+import java.io.IOException
 import java.util.regex.Pattern
 
 object VideoUtils {
@@ -13,7 +14,7 @@ object VideoUtils {
     @WorkThread
     @Throws(Exception::class)
     fun getVideoInfo(video: File): VideoInfoBean {
-        val result = SystemUtils.execute("ffprobe", "-v", "quiet", "-print_format", "json", "-show_format", "-show_streams", video.absolutePath)
+        val result = ProcessUtils.execute("ffprobe", "-v", "quiet", "-print_format", "json", "-show_format", "-show_streams", video.absolutePath)
         return Gson().fromJson(StringUtils.join<Any>(*result.toTypedArray()), VideoInfoBean::class.java)
     }
 
@@ -22,21 +23,22 @@ object VideoUtils {
     fun getFrameCount(video: File): Int {
         val videoInfo = getVideoInfo(video)
         val duration = videoInfo.format!!.duration!!.toDouble()
-        val fpsString = videoInfo.streams!![0]!!.rFrameRate
+        val fpsString = videoInfo.streams!![0]!!.rFrameRate!!
         val matcher = FPS_PATTERN.matcher(fpsString)
         if (matcher.matches()) {
             val fps = matcher.group("a").toDouble() / matcher.group("b").toDouble()
             return (fps * duration).toInt()
         }
-        throw Exception("error video info: $video")
+
+        throw IOException("error video info: $video")
     }
 
     private val FPS_PATTERN = Pattern.compile("^(?<a>[0-9]+)/(?<b>[0-9]+)$")
 
-    @WorkThread
-    fun videoFilter(video: File, scale: Int): List<String> {
-        return listOf("ffmpeg", "-i", video.absolutePath, "-vf", "scale=$scale:$scale,transpose=1")
-    }
+//    @WorkThread
+//    fun videoFilter(video: File, scale: Int): List<String> {
+//        return listOf("ffmpeg", "-i", video.absolutePath, "-vf", "scale=$scale:$scale,transpose=1")
+//    }
 
     private val FRAME_PATTERN = Pattern.compile("^frame=(?<frame>[ 0-9]+)fps.*")
 
